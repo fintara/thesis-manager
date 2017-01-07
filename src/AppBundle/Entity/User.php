cd3fb53960db4110961f5ef77c852cac52a1c0a2
Tsvetan Ovedenski <dev@tsovedenski.com>
2017-01-07 15:34:18 +0100

    Add Student and Worker user types

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,12 +3,18 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ * @ORM\InheritanceType(value="SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="type", type="string")
+ * @ORM\DiscriminatorMap({
+ *     "student" = "AppBundle\Entity\Student",
+ *     "worker" = "AppBundle\Entity\Worker"
+ * })
  */
-class User
+abstract class User implements UserInterface
 {
     /**
      * @var int
@@ -139,6 +145,39 @@ class User
     public function getRoles(): array
     {
         return $this->roles;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return '';
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        $this->plainPassword = '';
     }
 }
 
