6da4c18fdeb73331d35b45005a0a19e5b32be329
Tsvetan Ovedenski <dev@tsovedenski.com>
2017-01-16 22:33:42 +0100

    Add test stub

---
+++ tests/AppBundle/Services/TopicServiceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\AppBundle\Services;
+
+use AppBundle\Entity\Student;
+use AppBundle\Entity\Topic;
+use AppBundle\Services\TopicService;
+
+class TopicServiceTest extends \PHPUnit_Framework_TestCase
+{
+    private $service;
+    private $student;
+    private $topic;
+
+    protected function setUp()
+    {
+        $this->service = new TopicService($this->topic, $this->student);
+    }
+
+    public function testReserveFirstTime()
+    {
+
+    }
+
+}
