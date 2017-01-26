bb091c7116c6c78badc3a023d5357decfc863fdd
Tsvetan Ovedenski <dev@tsovedenski.com>
2017-01-26 12:07:44 +0100

    Add Draft service tests

---
+++ tests/AppBundle/Services/DraftServiceTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fintara
+ * Date: 26/01/2017
+ * Time: 12:01
+ */
+
+namespace AppBundle\Services;
+
+
+use AppBundle\Entity\Student;
+use AppBundle\Entity\Thesis;
+use AppBundle\Models\DraftModel;
+use AppBundle\Repository\DraftRepositoryInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class DraftServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var DraftService */
+    private $service;
+
+    protected function setUp()
+    {
+        $repo = $this->createMock(DraftRepositoryInterface::class);
+        $repo->method('findLastVersion')->willReturn(5);
+
+        $this->service = new DraftService($repo);
+    }
+
+    public function testCreateDraft()
+    {
+        $model = new DraftModel();
+        $model->comment = 'Test comment';
+        $model->file = new UploadedFile(__DIR__.'/../../dummy_file.zip', '');
+        $model->student = new Student();
+        $model->thesis = new Thesis();
+
+        $obj = $this->service->create($model);
+
+        $this->assertEquals(6, $obj->getVersion());
+    }
+}
